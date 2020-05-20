3f368a0a70f00a314f171423662c6562c4a27d68
Pineda A <32521420+GalPin-Ark@users.noreply.github.com>
2020-05-20 13:29:24 -0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,56 +3,44 @@ import { Link, graphql } from 'gatsby'
 import Masonry from 'react-masonry-component'
 import Img from 'gatsby-image'
 import Layout from "../components/layout"
-import { injectIntl } from "react-intl";
 
-class IndexPage extends React.Component {
-  render() {
-    let { data } = this.props;
-    const prefix =
-      this.props.intl.locale === "es" ? "" : `/${this.props.intl.locale}`;
-    return (
-      <Layout>
-      <Masonry className="showcase">
-        {data.allDatoCmsWork.edges.map(({ node: work }) => (
-          <div key={work.id} className="showcase__item">
-            <figure className="card">
-              <Link to={`${prefix}/works/${work.slug}`} className="card__image">
-                <Img sizes={work.coverImage.sizes} />
-              </Link>
-              <figcaption className="card__caption">
-                <h6 className="card__title">
-                  <Link to={`${prefix}/works/${work.slug}`}>{work.title}</Link>
-                </h6>
-                <div className="card__description">
-                  <p>{work.excerpt}</p>
-                </div>
-              </figcaption>
-            </figure>
-          </div>
-        ))}
-      </Masonry>
-      </Layout>
-    );
-  }
-}
+const IndexPage = ({ data }) => (
+  <Layout>
+    <Masonry className="showcase">
+      {data.allDatoCmsWork.edges.map(({ node: work }) => (
+        <div key={work.id} className="showcase__item">
+          <figure className="card">
+            <Link to={`/works/${work.slug}`} className="card__image">
+              <Img fluid={work.coverImage.fluid} />
+            </Link>
+            <figcaption className="card__caption">
+              <h6 className="card__title">
+                <Link to={`/works/${work.slug}`}>{work.title}</Link>
+              </h6>
+              <div className="card__description">
+                <p>{work.excerpt}</p>
+              </div>
+            </figcaption>
+          </figure>
+        </div>
+      ))}
+    </Masonry>
+  </Layout>
+)
 
-export default injectIntl(IndexPage);
+export default IndexPage
 
 export const query = graphql`
-  query IndexQuery($locale: String!) {
-    allDatoCmsWork(
-      filter: { locale: { eq: $locale } }
-      sort: { fields: [position], order: ASC }
-    ) {
+  query IndexQuery {
+    allDatoCmsWork(sort: { fields: [position], order: ASC }) {
       edges {
         node {
           id
           title
           slug
-          locale
           excerpt
           coverImage {
-            sizes(maxWidth: 450, imgixParams: { fm: "jpg", auto: "compress" }) {
+            fluid(maxWidth: 450, imgixParams: { fm: "jpg", auto: "compress" }) {
               ...GatsbyDatoCmsSizes
             }
           }
@@ -60,49 +48,4 @@ export const query = graphql`
       }
     }
   }
-`;
-// const IndexPage = ({ data }) => (
-//   <Layout>
-//     <Masonry className="showcase">
-//       {data.allDatoCmsWork.edges.map(({ node: work }) => (
-//         <div key={work.id} className="showcase__item">
-//           <figure className="card">
-//             <Link to={`/works/${work.slug}`} className="card__image">
-//               <Img fluid={work.coverImage.fluid} />
-//             </Link>
-//             <figcaption className="card__caption">
-//               <h6 className="card__title">
-//                 <Link to={`/works/${work.slug}`}>{work.title}</Link>
-//               </h6>
-//               <div className="card__description">
-//                 <p>{work.excerpt}</p>
-//               </div>
-//             </figcaption>
-//           </figure>
-//         </div>
-//       ))}
-//     </Masonry>
-//   </Layout>
-// )
-
-// export default IndexPage
-
-// export const query = graphql`
-//   query IndexQuery {
-//     allDatoCmsWork(sort: { fields: [position], order: ASC }) {
-//       edges {
-//         node {
-//           id
-//           title
-//           slug
-//           excerpt
-//           coverImage {
-//             fluid(maxWidth: 450, imgixParams: { fm: "jpg", auto: "compress" }) {
-//               ...GatsbyDatoCmsSizes
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
+`
