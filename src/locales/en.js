bb88477f98def181101f25fddad0f009c0ad74a4
Pineda A <32521420+GalPin-Ark@users.noreply.github.com>
2020-05-20 10:45:17 -0500

    update locale

---
+++ src/locales/en.js
@@ -0,0 +1,10 @@
+const messages = {
+    'menu.about': 'About',
+    'menu.home': 'Home',
+    'menu': 'Menu:',
+    'langs': 'Languages:',
+    'lang_es': 'ESP',
+    'lang_en': 'ENG'
+  };
+  
+  export default messages;
---
+++ src/locales/es.js
@@ -0,0 +1,10 @@
+const messages = {
+    'menu.about': 'Acerca de',
+    'menu.home': 'Inico',
+    'menu': 'Menu:',
+    'langs': 'Lenguajes:',
+    'lang_es': 'ESP',
+    'lang_en': 'ENG'
+  };
+  
+  export default messages;
